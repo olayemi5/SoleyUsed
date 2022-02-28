2e31913229d6027697ffab1be7d18e6af68eacdf
Stephen Olayemi <olayemistephen007@gmail.com>
2022-02-28 11:59:19 +0100

    Initail commit

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let x = 1;
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World from first app</Text>
       <StatusBar style="auto" />
     </View>
   );
